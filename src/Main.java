d8150793a8f913f3c5238d0eadf7cc09dc41f0f9
esteves-dev <matheusesteves734@gmail.com>
2025-08-18 23:34:11 -0300

    Adiciona programa de teste com Scanner

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner ler = new Scanner(System.in);
+        System.out.print("Nome: ");
+        String nome = ler.nextLine();
+        System.out.print("Idade: ");
+        int idade = ler.nextInt();
+        System.out.print("Nota: ");
+        float nota = ler.nextFloat();
+
+        System.out.println("Nome: " + nome + ", Idade: " + idade + ", Nota: " + nota);
+
+    }
+}
