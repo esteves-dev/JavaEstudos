b5537751b452d517dec505e57840cb2e3738d2e1
esteves-dev <matheusesteves734@gmail.com>
2025-08-23 17:10:31 -0300

    Programa com loops aninhados (tabela alinhada)

---
+++ src/TabelaMultiplicacaoAninhada.java
@@ -0,0 +1,16 @@
+public class TabelaMultiplicacaoSimples {
+    public static void main(String[] args) {
+            System.out.printf("%4s", "");
+            for (int s = 1; s <= 5; s++){
+                System.out.printf("%4d", s);
+            }
+
+            for (int i = 1; i <= 5; i++) {
+                System.out.printf("\n%4d", i);
+                for (int j = 1; j <= 5; j++){
+                    System.out.printf("%4d", i*j);
+            }
+        }
+        
+    }
+    }
