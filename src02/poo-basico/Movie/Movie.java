8fd386e8167e0e9072014c59b6cea8b083c1eb3e
Matheus Esteves <matheusesteves734@gmail.com>
2025-10-11 19:45:24 -0300

    Movie class

---
+++ src02/poo-basico/Movie/Movie.java
@@ -0,0 +1,15 @@
+class Movie{
+    String title;
+    String genre;
+    int year;
+
+    void getAgeOfMovie(int currentYear){
+        System.out.printf("O filme '%s' tem %d anos.\n", title, currentYear - year);
+    }
+
+    void showInfo(){
+        System.out.println("Title: " + title);
+        System.out.println("Genre: " + genre);
+        System.out.println("Year: " + year);
+    }
+}
