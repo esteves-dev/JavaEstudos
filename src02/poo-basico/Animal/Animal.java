d519b1f04360a3d1d4953a765339e2ecd1b649f2
Matheus Esteves <matheusesteves734@gmail.com>
2025-10-11 19:54:44 -0300

    Animal class

---
+++ src02/poo-basico/Animal/Animal.java
@@ -0,0 +1,20 @@
+class Animal{
+    String name;
+    String species;
+    int age;
+
+    void showInfo(){
+        System.out.println("Name: " + name);
+        System.out.println("Species: " + species);
+        System.out.println("Age: " + age + "years old");
+    }
+
+    void makeSound(){
+        if (species.equals("Gato") || species.equals("gato") || species.equals("cat") || species.equals("Cat")){
+            System.out.println("Miau!");
+        }
+        else if (species.equals("Cachorro") || species.equals("cachorro") || species.equals("dog") || species.equals("Dog")){
+            System.out.println("Au! Au!");
+        }
+    }
+}
