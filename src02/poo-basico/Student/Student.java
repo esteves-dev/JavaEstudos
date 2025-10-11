9b3881aacf1af0ec7726212eb8a9e904349cc723
Matheus Esteves <matheusesteves734@gmail.com>
2025-10-11 19:42:33 -0300

    Student class

---
+++ src02/poo-basico/Student/Student.java
@@ -0,0 +1,11 @@
+class Student {
+    String name;
+    int age;
+    double grade;
+
+    void showInfo(){
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+    }
+}
