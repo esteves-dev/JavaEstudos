598c2e8263f5251a5a5fdf996d7e6d2a11b380ca
Matheus Esteves <matheusesteves734@gmail.com>
2025-10-11 19:55:03 -0300

    Animal main class

---
+++ src02/poo-basico/Animal/AnimalMain.java
@@ -0,0 +1,16 @@
+public class AnimalMain {
+    public static void main(String[] args) {
+        Animal a1 = new Animal();
+        a1.name = "Mel";
+        a1.species = "Cachorro";
+        a1.age = 5;
+        a1.makeSound();
+        a1.showInfo();
+        Animal a2 = new Animal();
+        a2.name = "Pitico";
+        a2.species = "Gato";
+        a2.age = 7;
+        a2.makeSound();
+        a2.showInfo();
+    }
+}
