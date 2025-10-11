9bae4a9fff184c46e3f144557cdef292e7f5cdcd
Matheus Esteves <matheusesteves734@gmail.com>
2025-10-11 19:43:22 -0300

    Student main class

---
+++ src02/poo-basico/Student/StudentMain.java
@@ -0,0 +1,15 @@
+public class StudentMain{
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Matheus Esteves";
+        s1.age = 15;
+        s1.grade = 75;
+        s1.showInfo();
+
+        Student s2 = new Student();
+        s2.name = "Bianca Andrade";
+        s2.age = 15;
+        s2.grade = 100;
+        s2.showInfo();
+    }
+}
