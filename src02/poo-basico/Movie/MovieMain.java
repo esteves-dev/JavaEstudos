b93311c5995a307e60fa87a62cbc46cadd5e6ba4
Matheus Esteves <matheusesteves734@gmail.com>
2025-10-11 19:45:55 -0300

    Movie main class

---
+++ src02/poo-basico/Movie/MovieMain.java
@@ -0,0 +1,16 @@
+public class MovieMain {
+    public static void main(String[] args) {
+        Movie m1 = new Movie();
+        m1.title = "Homem-Aranha 2";
+        m1.genre = "Herói";
+        m1.year = 2004;
+        m1.showInfo();
+        m1.getAgeOfMovie(2025);
+        Movie m2 = new Movie();
+        m2.title = "Homem-Aranha 1";
+        m2.genre = "Herói";
+        m2.year = 2002;
+        m2.showInfo();
+        m2.getAgeOfMovie(2025);
+    }
+}
