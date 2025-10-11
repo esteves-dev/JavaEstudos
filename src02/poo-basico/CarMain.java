4f25ab6e0feaad0171d9ebdfaa63aec9d52e167a
Matheus Esteves <matheusesteves734@gmail.com>
2025-10-11 19:48:54 -0300

    Car main class

---
+++ src02/poo-basico/CarMain.java
@@ -0,0 +1,24 @@
+class Car{
+    String brand;
+    String model;
+    int year;
+    String color;
+    double speed;
+
+    void accelerate (double value){
+        speed += value;
+    }
+
+    void brake(double value){
+        speed -= value;
+        if (speed < 0){
+            System.out.println("ERRO!! Não é possível ter velocidade negativa, então foi ajustada para 0.");
+            speed = 0;
+        }
+    }
+
+    void showInfo(){
+        System.out.printf("%s %s %d %s \n", brand, model, year, color);
+        System.out.println("Speed: " + speed);
+    }
+}
